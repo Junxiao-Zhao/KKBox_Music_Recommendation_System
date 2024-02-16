bb0dc3fd1a0ae5854b5a28229956d5759f2bd2a2
Junxiao Zhao <zhao_junxiao@hotmail.com>
2024-02-15 17:56:08 -0800

    BERT4Rec SequenceDataset

--- src/BERT4Rec/tokenizer/tokenizer.py
+++ src/BERT4Rec/tokenizer/tokenizer.py
@@ -1,5 +1,6 @@
 import os
 import json
+import random
 from typing import Dict, List, Iterable
 
 import torch
@@ -10,6 +11,7 @@ from sklearn.preprocessing import MultiLabelBinarizer
 
 
 class Tokenizer:
+    """(Ke)BERT4Rec Tokenizer"""
 
     def __init__(self,
                  vocab: Dict[str, Dict],
@@ -43,13 +45,94 @@ class Tokenizer:
         }
         item2id['[PAD]'] = 0
         item2id['[MASK]'] = 1
-        id2item = {idx: item for item, idx in item2id.items()}
+        id2item = {str(idx): item for item, idx in item2id.items()}
 
+        mlb = None
         if keyword_ls is not None:
             mlb = MultiLabelBinarizer()
             mlb.fit(keyword_ls)
 
         return cls({'item2id': item2id, 'id2item': id2item}, mlb)
+
+    @staticmethod
+    def padding(tokens: Iterable[str], side: str = 'left', max_len: int = 100):
+        """Padding
+
+        :param tokens: a sequence of tokens
+        :param side: 'left' or 'right'; default 'left'
+        :param max_len: the max sequence length
+        :return: a sequence of tokens after padding
+        """
+
+        tokens = list(tokens)[-max_len:]
+        if side == 'left':
+            pad_tokens = ['[PAD]'] * (max_len - len(tokens)) + tokens
+        elif side == 'right':
+            pad_tokens = tokens + ['[PAD]'] * (max_len - len(tokens))
+        else:
+            raise ValueError('side should be either "left" or "right"')
+
+        return pad_tokens
+
+    @staticmethod
+    def mask(tokens: Iterable[str], ratio: float = 0.2):
+        """Mask randomly
+
+        :param tokens: a sequence of tokens
+        :param ration: the mask ratio
+        :return: a sequence of masked tokens
+        """
+
+        tokens_list = list(tokens)
+        total_tokens = len(tokens_list)
+        num_to_mask = round(total_tokens * ratio)
+        indices_to_mask = random.sample(range(total_tokens), num_to_mask)
+
+        masked_tokens = [
+            '[MASK]' if idx in indices_to_mask else token
+            for idx, token in enumerate(tokens_list)
+        ]
+
+        return masked_tokens
+
+    def convert_tokens_to_ids(self, tokens: Iterable[str]):
+        """Convert tokens into ids
+
+        :param tokens: a sequence of tokens
+        :return: a tensor of ids
+        """
+
+        return torch.tensor(list(
+            map(lambda item: self.vocab['item2id'].get(item, 1), tokens)),
+                            dtype=torch.long)
+
+    def convert_ids_to_tokens(self, ids: Iterable[int]) -> List[str]:
+        """Convert ids into tokens
+
+        :param ids: a sequence of ids
+        :return: a list of tokens
+        """
+
+        return list(map(lambda idx: self.vocab['id2item'][str(idx)], ids))
+
+    def encode_keywords(self, keyword_ls: Iterable[Iterable[str]]):
+        """Encode keywords using multi-hot encoding
+
+        :param keyword_ls: a set of keywords for each item
+        :return: a tensor of the multi-hot encodings
+        """
+
+        return torch.tensor(self.keyword_encoder.transform(keyword_ls),
+                            dtype=torch.long)
+
+    def decode_keywords(self, mulit_hot: np.ndarray):
+        """Decode multi-hot encoding into keywords
+
+        :param mulit_hot: a tensor of multi-hot encodings
+        :return: a set of keywords for each item
+        """
+
+        return self.keyword_encoder.inverse_transform(mulit_hot)
 
     @classmethod
     def load(cls,
@@ -75,6 +158,7 @@ class Tokenizer:
         with open(vocab_fp, 'r', encoding='utf-8') as vocab_f:
             vocab = json.load(vocab_f)
 
+        kw_enc = None
         if load_kw_enc:
             if not keyword_enc_fp:
                 keyword_enc_fp = os.path.join(os.path.dirname(__file__),
@@ -87,6 +171,12 @@ class Tokenizer:
         return cls(vocab, kw_enc)
 
     def save(self, vocab_fp: str = None, keyword_enc_fp: str = None):
+        """Save the Tokenizer
+
+        :param vocab_fp: the vocab's file path; if None, use the default
+        :param keyword_enc_fp: the multi-hot encoder's file path;
+        if None, use the default
+        """
 
         if not vocab_fp:
             vocab_fp = os.path.join(os.path.dirname(__file__), 'vocab.json')
---
+++ src/BERT4Rec/train.py
@@ -0,0 +1,88 @@
+import random
+
+import pandas as pd
+import torch
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+from pandas.core.groupby.generic import DataFrameGroupBy
+
+from . import Tokenizer
+
+
+class SequenceDataset(Dataset):
+    """Sequence Dataset for KeBERT4Rec"""
+
+    def __init__(self,
+                 group_by: DataFrameGroupBy,
+                 mode: str,
+                 tokenizer: Tokenizer,
+                 seq_len: int = 100) -> None:
+        """Constructor
+
+        :param group_by: a pandas groupby
+        :param mode: 'train' or 'val'
+        :param tokenizer: the Tokenizer
+        :param seq_len: the sequence length
+        """
+
+        self.groups = list(group_by.groups.keys())
+        self.group_by = group_by
+        self.mode = mode
+        self.tokenizer = tokenizer
+        self.seq_len = seq_len
+
+    def __len__(self):
+        return len(self.groups)
+
+    def __getitem__(self, index):
+
+        group = self.groups[index]
+        group_df: pd.DataFrame = self.group_by.get_group(group)
+        group_df = group_df.drop_duplicates(subset='song_id',
+                                            ignore_index=True)
+
+        # sample start and end
+        end_idx = random.randint(
+            10,
+            group_df.shape[0]) if self.mode == 'train' else group_df.shape[0]
+        start_idx = max(0, end_idx - self.seq_len)
+        group_df = group_df.iloc[start_idx:end_idx]
+
+        # extract items and keywords
+        target_items = group_df['song_id'].to_list()
+        keywords_ls = group_df['genre_ids'].astype(str).str.split('|')
+        target_keywords_tensor = self.tokenizer.encode_keywords(keywords_ls)
+
+        # mask
+        if self.mode == 'train':
+            # only mask the last item, akin finetune
+            if random.random() <= 0.05:
+                source_items = target_items[:-1] + ['[MASK]']
+            else:
+                source_items = self.tokenizer.mask(target_items, ratio=0.2)
+        else:
+            source_items = target_items[:] + ['[MASK]']
+
+        # padding
+        pad_len = self.seq_len - len(target_items)
+        pad_side = 'left' if random.random() <= 0.5 else 'right'
+        target_items = self.tokenizer.padding(target_items,
+                                              side=pad_side,
+                                              max_len=self.seq_len)
+        source_items = self.tokenizer.padding(source_items,
+                                              side=pad_side,
+                                              max_len=self.seq_len)
+        target_keywords_tensor = F.pad(target_keywords_tensor,
+                                       (0, 0, pad_len,
+                                        0) if pad_side == 'left' else
+                                       (0, 0, 0, pad_len))
+
+        # convert to tensors
+        target_item_tensor = self.tokenizer.convert_tokens_to_ids(target_items)
+        source_item_tensor = self.tokenizer.convert_tokens_to_ids(source_items)
+        mask = (target_item_tensor != source_item_tensor).to(torch.long)
+        source_keywords_tensor = target_keywords_tensor.masked_fill(
+            mask.unsqueeze(1) == 1, 1)  # mask keywords
+
+        return (source_item_tensor, target_item_tensor, mask,
+                source_keywords_tensor, target_keywords_tensor)
