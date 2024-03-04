7f85398019599653801bffca1892d7da4a2dfdd9
thomas1728 <zzhu20@uci.edu>
2024-03-03 17:25:02 -0800

    minor changes in DCN.train.py

--- src/DCN/train.py
+++ src/DCN/train.py
@@ -1,12 +1,10 @@
 import os
 import sys
 
-
 parent_dir = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
 if parent_dir not in sys.path:
     sys.path.append(parent_dir)
 
-import joblib
 import pandas as pd
 import numpy as np
 import torch
@@ -15,6 +13,7 @@ import torch.nn.functional as F
 
 from deepctr_torch.inputs import SparseFeat, DenseFeat, get_feature_names, VarLenSparseFeat
 from deepctr_torch.models import DCN
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 from deepctr_torch.callbacks import EarlyStopping, ModelCheckpoint
 
@@ -54,18 +53,19 @@ if __name__ == "__main__":
     # preprocess dataset and assign features
     sparse_features = [
         'msno', 'song_id', 'source_system_tab', 'source_screen_name',
-        'source_type', 'artist_name', 'composer', 'lyricist', 'language', 'city',
-        'gender', 'registered_via'
+        'source_type', 'artist_name', 'composer', 'lyricist', 'language',
+        'city', 'gender', 'registered_via'
     ]
     dense_features = ['song_length', 'bd', 'duration']
     fixlen_feature_columns = [
-                                 SparseFeat(feat, preprocesser.vocab_size[feat], embedding_dim=64)
-                                 for feat in sparse_features
-                             ] + [DenseFeat(feat, 1) for feat in dense_features]
+        SparseFeat(feat, preprocesser.vocab_size[feat], embedding_dim=64)
+        for feat in sparse_features
+    ] + [DenseFeat(feat, 1) for feat in dense_features]
 
     varlen_feature_columns = [
         VarLenSparseFeat(SparseFeat('genre_ids',
-                                    vocabulary_size=len(preprocesser.genre2idx),
+                                    vocabulary_size=len(
+                                        preprocesser.genre2idx),
                                     embedding_dim=64),
                          maxlen=preprocesser.genre_maxlen,
                          combiner='mean')
@@ -73,7 +73,8 @@ if __name__ == "__main__":
 
     linear_feature_columns = fixlen_feature_columns + varlen_feature_columns
     dnn_feature_columns = fixlen_feature_columns + varlen_feature_columns
-    feature_names = get_feature_names(linear_feature_columns + dnn_feature_columns)
+    feature_names = get_feature_names(linear_feature_columns +
+                                      dnn_feature_columns)
 
     tr_model_input = {name: tr_song_msno_df[name] for name in feature_names}
     val_model_input = {name: val_song_msno_df[name] for name in feature_names}
@@ -81,25 +82,30 @@ if __name__ == "__main__":
     val_model_input['genre_ids'] = val_genre
 
     # train model
-    model = DCN(linear_feature_columns, dnn_feature_columns, task='binary', l2_reg_embedding=1e-5, device=device)
-    es = EarlyStopping(monitor='val_auc', min_delta=0, verbose=1, patience=1, mode='min')
-    mdckpt = ModelCheckpoint(filepath='checkpoints/DCN/model.ckpt', monitor='val_auc', verbose=1, save_best_only=True,
-                             mode='min')
+    model = DCN(linear_feature_columns,
+                dnn_feature_columns,
+                task='binary',
+                l2_reg_embedding=1e-5,
+                device=device)
+    es = EarlyStopping(monitor='val_auc',
+                       min_delta=0,
+                       verbose=1,
+                       patience=3,
+                       mode='max')
+    os.makedirs('../../checkpoints/DCN/', exist_ok=True)
+    mdckpt = ModelCheckpoint(filepath='../../checkpoints/DCN/model.ckpt',
+                             monitor='val_auc',
+                             verbose=1,
+                             save_best_only=True,
+                             mode='max')
 
     model.compile("adam", "binary_crossentropy", metrics=['auc'])
-    history = model.fit(tr_model_input, tr_song_msno_df['target'].values,
-                        batch_size=80,
+    history = model.fit(tr_model_input,
+                        tr_song_msno_df['target'].values,
+                        batch_size=64,
                         epochs=10,
                         verbose=1,
-                        validation_data=(val_model_input, val_song_msno_df['target'].values),
+                        validation_data=(val_model_input,
+                                         val_song_msno_df['target'].values),
                         shuffle=True,
-                        callbacks=[es,mdckpt])
-
-
-
-
-
-
-
-
-
+                        callbacks=[es, mdckpt])
