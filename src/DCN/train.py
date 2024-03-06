cdd5d2e24b759f784feb016002d4d20cba190e46
thomas1728 <zzhu20@uci.edu>
2024-03-05 17:17:38 -0800

    "final" version of DCN.train.py

--- src/DCN/train.py
+++ src/DCN/train.py
@@ -1,96 +1,88 @@
 import os
 import sys
+import json
+
+import ast
+import torch
+import torch.optim as optim
+import pandas as pd
+from tensorflow.keras.utils import pad_sequences
+from deepctr_torch.models import DCN
+from deepctr_torch.inputs import (SparseFeat, DenseFeat, VarLenSparseFeat,
+                                  get_feature_names)
+from deepctr_torch.callbacks import EarlyStopping, ModelCheckpoint
 
 parent_dir = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
 if parent_dir not in sys.path:
     sys.path.append(parent_dir)
 
-import pandas as pd
-import numpy as np
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
+from preprocess import preprocess, generate_datasets
 
-from deepctr_torch.inputs import SparseFeat, DenseFeat, get_feature_names, VarLenSparseFeat
-from deepctr_torch.models import DCN
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-from deepctr_torch.callbacks import EarlyStopping, ModelCheckpoint
-
-parent_dir = os.path.abspath(os.path.join(os.getcwd(), os.pardir, os.pardir))
-if parent_dir not in sys.path:
-    sys.path.append(parent_dir)
-
-from src.preprocess import Preprocesser
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 if __name__ == "__main__":
-    train_df = pd.read_csv("../../data/sampled_train.csv")
-    val_df = pd.read_csv("../../data/pseudo_test.csv")
-    songs_df = pd.read_csv("../../data/songs.csv")
-    song_extra_info_df = pd.read_csv("../../data/song_extra_info.csv")
-    members_df = pd.read_csv("../../data/members.csv")
-    real_train_df = pd.read_csv("../../data/train.csv")
-    real_test_df = pd.read_csv("../../data/test.csv")
+    # load data
+    train_df = pd.read_csv('../../data/train.csv')
+    test_df = pd.read_csv('../../data/test.csv')
+    songs_df = pd.read_csv('../../data/songs.csv')
+    members_df = pd.read_csv('../../data/members.csv')
 
-    preprocesser = Preprocesser()
-    preprocesser.fit_train_test(pd.concat([real_train_df, real_test_df]))
-    preprocesser.transform_train_test(train_df)
-    preprocesser.transform_train_test(val_df)
-    preprocesser.preprocess_songs(songs_df)
-    preprocesser.preprocess_members(members_df)
+    tr_song_msno_df, val_song_msno_df, ts_song_msno_df, item2idx = preprocess(
+        train_df, test_df, songs_df, members_df)
 
-    # merge data and use label encoder
-    tr_song_df = train_df.merge(songs_df, how='inner', on='song_id')
-    tr_song_msno_df = tr_song_df.merge(members_df, how='inner', on='msno')
-    val_song_df = val_df.merge(songs_df, how='inner', on='song_id')
-    val_song_msno_df = val_song_df.merge(members_df, how='inner', on='msno')
-    preprocesser.transform_msno_song(tr_song_msno_df)
-    preprocesser.transform_msno_song(val_song_msno_df)
-
-    tr_genre = preprocesser.padding_genre(tr_song_msno_df['genre_ids'])
-    val_genre = preprocesser.padding_genre(val_song_msno_df['genre_ids'])
-
-    # preprocess dataset and assign features
+    # dataset and features
     sparse_features = [
-        'msno', 'song_id', 'source_system_tab', 'source_screen_name',
-        'source_type', 'artist_name', 'composer', 'lyricist', 'language',
-        'city', 'gender', 'registered_via'
+        'msno',
+        'song_id',
+        'source_system_tab',
+        'source_screen_name',
+        'source_type',
+        # 'composer',
+        # 'lyricist',
+        'language',
+        'is_featured',
+        'city',
+        'registered_via',
+        'gender',
     ]
-    dense_features = ['song_length', 'bd', 'duration']
-    fixlen_feature_columns = [
-        SparseFeat(feat, preprocesser.vocab_size[feat], embedding_dim=64)
-        for feat in sparse_features
-    ] + [DenseFeat(feat, 1) for feat in dense_features]
-
-    varlen_feature_columns = [
-        VarLenSparseFeat(SparseFeat('genre_ids',
-                                    vocabulary_size=len(
-                                        preprocesser.genre2idx),
-                                    embedding_dim=64),
-                         maxlen=preprocesser.genre_maxlen,
-                         combiner='mean')
+    dense_features = [
+        'song_length',
+        # 'num_artist',
+        # 'num_composer',
+        # 'num_lyricist',
+        'bd',
+        'registration_init_time',
+        'expiration_date',
+        # 'duration',
+    ]
+    varlen_features = [
+        'genre_ids',
+        'artist_name',
     ]
 
-    linear_feature_columns = fixlen_feature_columns + varlen_feature_columns
-    dnn_feature_columns = fixlen_feature_columns + varlen_feature_columns
-    feature_names = get_feature_names(linear_feature_columns +
-                                      dnn_feature_columns)
+    (tr_model_input, val_model_input, ts_model_input, linear_feature_columns,
+     dnn_feature_columns) = generate_datasets(tr_song_msno_df,
+                                              val_song_msno_df,
+                                              ts_song_msno_df,
+                                              sparse_features,
+                                              dense_features,
+                                              varlen_features,
+                                              item2idx,
+                                              embed_dim=64)
 
-    tr_model_input = {name: tr_song_msno_df[name] for name in feature_names}
-    val_model_input = {name: val_song_msno_df[name] for name in feature_names}
-    tr_model_input['genre_ids'] = tr_genre
-    val_model_input['genre_ids'] = val_genre
+    model = model = DCN(linear_feature_columns,
+                   dnn_feature_columns,
+                   task='binary',
+                   l2_reg_embedding=1e-5,
+                   dnn_dropout=0.3,
+                   l2_reg_dnn=1e-5,
+                   dnn_use_bn=True,
+                   device=DEVICE)
 
-    # train model
-    model = DCN(linear_feature_columns,
-                dnn_feature_columns,
-                task='binary',
-                l2_reg_embedding=1e-5,
-                device=device)
     es = EarlyStopping(monitor='val_auc',
                        min_delta=0,
                        verbose=1,
-                       patience=3,
+                       patience=2,
                        mode='max')
     os.makedirs('../../checkpoints/DCN/', exist_ok=True)
     mdckpt = ModelCheckpoint(filepath='../../checkpoints/DCN/model.ckpt',
@@ -99,12 +91,16 @@ if __name__ == "__main__":
                              save_best_only=True,
                              mode='max')
 
-    model.compile("adam", "binary_crossentropy", metrics=['auc'])
+    model.compile(
+        # optimizer="adam",
+        optimizer=optim.RMSprop(model.parameters(), lr=1e-3),
+        loss="binary_crossentropy",
+        metrics=['auc'])
     history = model.fit(tr_model_input,
                         tr_song_msno_df['target'].values,
-                        batch_size=64,
+                        batch_size=8192,
                         epochs=10,
-                        verbose=1,
+                        verbose=2,
                         validation_data=(val_model_input,
                                          val_song_msno_df['target'].values),
                         shuffle=True,
